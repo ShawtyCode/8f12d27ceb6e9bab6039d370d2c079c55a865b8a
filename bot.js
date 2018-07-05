5a28ce7d86a8f460e8d020b873919a26af9af955
ShawtyCode <40865968+ShawtyCode@users.noreply.github.com>
2018-07-05 18:34:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,21 @@
 const discord = require("discord.js");
 
+
+const TOKEN = "MjI0MjAyMjE4MTkzMDI3MDcy.Dh_IjA.3KsO8apULPqgfmXKLti_RVg0kLI";
+
 const bot = new discord.Client();
 
 // add message as a parameter to your callback function
 bot.on('message', function(message) {
     // Now, you can use the message variable inside
-    if (message.content === "c!autobump") { 
+    if (message.content === "$loop") { 
         var interval = setInterval (function () {
             // use the message's channel (TextChannel) to send a new message
             message.channel.send("!disboard bump")
             .catch(console.error); // add error handling here
-        }, 1 * 120); 
+        }, 1 * 120000); 
     }
 });
 
-client.login(process.env.BOT_TOKEN);
 
-// You can run the program by typing node index in cmd
-// The program starts after the time interval is once over
-// That's all
-// You can press CTRL + C in command prompt to stop the program
-// Mine won't stop cause I used a host to keep it alive 24/7
-// You can search how to host a node.js app on YouTube.
+bot.login(TOKEN);
